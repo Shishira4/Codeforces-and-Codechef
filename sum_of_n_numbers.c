7e8506079516a01be3eef8353619ee12b3610686
Shishira S <69460743+Shishira4@users.noreply.github.com>
2021-01-30 12:06:29 +0530

    Added functions

--- sum_of_n_numbers.c
+++ sum_of_n_numbers.c
@@ -1,15 +1,36 @@
 #include<stdio.h>
+int input();
+int add(int n);
 int main()
 {
-	int n;
-	float sum = 0.0, value;
+    int n, sum =0;
+	n = input();
+	sum = add(n);
+}
+int input()
+{
+    int n;
 	printf("How many elements to add\n");
 	scanf("%d", &n);
-	printf("Enter the elements");
-	for(int i =0; i<=n; i++)
+	if(n<0)
+	input();
+	return n;
+}
+int add(int n)
+{
+	float s = 0.0, value;
+
+	printf("Enter the elements\n");
+	for(int i =0; i<n; i++)
 	{
 		scanf("%f",&value);
-		sum = sum+value;
+		s = s+value;
+    }
+printf("Sum of the integers is %f\n",s);
 }
-printf("Sum of the integers is %f\n",sum);
-}
+
+
+
+
+
+
