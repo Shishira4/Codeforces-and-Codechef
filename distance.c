6ec4d743e60358cb09bbbf79e7fd15f8edc4d196
Shishira S <69460743+Shishira4@users.noreply.github.com>
2021-01-30 12:35:57 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -2,22 +2,19 @@
 
 #include<stdio.h>
 #include<math.h>
-float input(char b, int i);  
-float distance(float x1, float x2, float y1, float y2); 
-void output(float x1, float x2, float y1, float y2, float dist); 
 float input(char b, int i)  
 {
 	float c;
 	printf("Enter the coordinate %c of point %i: ",b,i);
 	scanf("%f",&c);
 }
-float distance(float x1, float x2, float y1, float y2)  
+float distance(float x1, float y1,float x2, float y2)  
 {
 	float x;
 	x = sqrt((pow((x2-x1),2))+(pow((y2-y1),2)));
 	return x;
 }
-void output(float x1, float x2, float y1, float y2, float dist) 
+void output(float x1,float y1, float x2, float y2, float dist) 
 {
 	printf("The distance between %.2f, %.2f and %.2f,%.2f is: %.2f\n", x1,y1,x2,y2,dist);
 }
@@ -28,8 +25,7 @@ int main()
 	x2=input('x',2);
 	y1=input('y',1);
 	y2=input('y',2);
-	dist=distance(x1,x2,y1,y2);
-	output(x1,x2,y1,y2,dist);
+	dist=distance(x1,y1,x2,y2);
+	output(x1,y1,x2,y2,dist);
 	return 0;
 }
-
