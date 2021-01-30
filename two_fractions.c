30b71d26a58dce444ecf1d8e66eea7ab075863b7
Shishira S <69460743+Shishira4@users.noreply.github.com>
2021-01-30 10:13:30 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,30 +1,30 @@
 //WAP to find the sum of two fractions.
 #include<stdio.h>
-typedef struct
-     {
-        int n;
-        int d;
-     
-      }Fract;
- 
-   Fract sum(Fract,Fract);
- 
-   int main()
-    {
-     int num1,deno1,num2,deno2;
-     printf("Enter first fraction's numerator and denominator:");
-     scanf("%d%d",&num1,&deno1);
-     printf("Enter second fraction's numerator and denominator:");
-     scanf("%d%d",&num2,&deno2);
-    
-     Fract f1={num1, deno1}; // fraction 1
-     Fract f2 ={num2, deno2};//fraction 2
-     Fract result = sum(f1, f2);//sum the fractions
-     printf("Result=%d/%d",result.n,result.d);  //display the result
-     return 0;
-    }
-   Fract sum(Fract f1, Fract f2)
-    {
-     Fract result={(f1.n* f2.d) + (f2.n * f1.d), f1.d * f2.d};
-     return result;
-    }
+#include<math.h>
+float input(char b, int i)  
+{
+	float c;
+	printf("Enter the coordinate %c of point %i: ",b,i);
+	scanf("%f",&c);
+}
+float distance(float x1, float x2, float y1, float y2)  
+{
+	float x;
+	x = sqrt((pow((x2-x1),2))+(pow((y2-y1),2)));
+	return x;
+}
+void output(float x1, float x2, float y1, float y2, float dist) 
+{
+	printf("The distance between %.2f, %.2f and %.2f,%.2f is: %.2f\n", x1,y1,x2,y2,dist);
+}
+int main()  
+{
+	float x1,x2,y1,y2,dist;
+	x1=input('x',1);
+	x2=input('x',2);
+	y1=input('y',1);
+	y2=input('y',2);
+	dist=distance(x1,x2,y1,y2);
+	output(x1,x2,y1,y2,dist);
+	return 0;
+}
