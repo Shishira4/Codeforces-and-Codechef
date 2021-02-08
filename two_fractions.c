cbf2031a6aba9e2d589056064f98b2872bab3b57
Shishira S <69460743+Shishira4@users.noreply.github.com>
2021-02-08 12:46:58 +0530

    Updated compute_sum and corrected the gcd function

--- two_fractions.c
+++ two_fractions.c
@@ -1,25 +1,27 @@
-//WAP to find the sum of two fractions.
 #include<stdio.h>
 int compute_sum();
-typedef struct
+struct fract
     {
         int n;
         int d;
      
-    }Fract;
-void input()
+    };
+    typedef struct fract Fract;
+Fract input()
 {
-    int n1,d1,n2,d2;
-    Fract f1, f2;
-    printf("Enter the first numerator and denominator");
-    scanf("%d%d",&f1.n,&f1.d);
-    printf("Enter the second numerator and denominator");
-    scanf("%d%d",&f2.n,&f2.d);
-    compute_sum(f1,f2);
+    Fract x;
+    printf("Enter the numerator and denominator");
+    scanf("%d%d",&x.n,&x.d);
+    return x;
+ 
 }
 int main()
 {
-    input();
+    Fract f1,f2;
+    f1 = input();
+    f2 = input();
+    int d = compute_sum(f1,f2);
+
 }
 int gcd(int n, int d, Fract f1, Fract f2)
 {
@@ -34,7 +36,7 @@ int gcd(int n, int d, Fract f1, Fract f2)
         d = temp;
     }
     ans_n = ans_n/d;
-    ans_d = ans_n/d;
+    ans_d = ans_d/d;
     printf("The sum of the fractions %d/%d and %d/%d is %d/%d",f1.n,f1.d,f2.n,f2.d,ans_n,ans_d);
 }
 int compute_sum(Fract f1,Fract f2)
