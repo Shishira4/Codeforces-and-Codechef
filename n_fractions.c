140c3619f2e1e2764905bbce21a1b351fe45cae5
Shishira S <69460743+Shishira4@users.noreply.github.com>
2021-02-12 10:49:45 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,73 @@
 //WAP to find the sum of n fractions.
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct fract{
+  int n;
+  int d;
+};
+typedef struct fract Fract;
+Fract input();
+Fract sum(Fract f1, Fract f2);
+int gcd(int p, int q);
+int  result(Fract s);
+
+
+int main(){
+  int n;
+  printf("Enter the no. of terms: ");
+  scanf("%d",&n);
+  Fract x,y,a[n];
+  for(int i=0;i<n;i++){
+    printf("Enter fraction %d\n",i+1);
+    a[i] = input();
+  }
+  if(n==1){
+    y =a[0];
+  }
+  else{
+    x = a[0];
+    for(int i=0;i<n-1;i++){
+      y = sum(x,a[i+1]);
+      x = y;
+    }
+  }
+  result(x);
+  return 0;
+}
+
+Fract input(){
+  Fract fraction;
+  printf("Enter the numerator");
+  scanf("%d",&fraction.n);
+  printf("Enter the denominator");
+  scanf("%d",&fraction.d);
+  return fraction;
+}
+
+int gcd(int p, int q){
+  int a;
+  for(int i = 1; i <= p && i <= q; i++)
+    {
+        if(p % i == 0 && q % i == 0)
+            a = i;
+    }
+    return a;
+}
+
+Fract sum(Fract f1, Fract f2){
+  Fract f3;
+  int c;
+  f3.n = ((f1.n*f2.d)+(f2.n*f1.d));
+  f3.d = f1.d*f2.d;
+  c = gcd(f3.n,f3.d);
+  f3.n = f3.n/c;
+  f3.d = f3.d/c;
+  return f3;
+}
+
+int result(Fract s){
+  printf("The sum of the given fractions : %d / %d\n",s.n,s.d);
+  return 0;
+}
