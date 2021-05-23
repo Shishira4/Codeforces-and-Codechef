5f16a724d9a509a44883f9af708110fbdd093080
Shishira4 <shivashishira@gmail.com>
2021-05-23 10:47:46 +0530

    memory leak prgram

---
+++ incrementalmemoryleak.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void calculate(int memory1, int memeory2)
+{
+    for(int time =1; max(memory1,memory2); time++)
+    {
+
+    }
+}
+int main()
+{
+    int memory1, memory2;
+    scanf("%d %d",&memory1,&memory2);
+    calculate(memory1, memory2);
+}
